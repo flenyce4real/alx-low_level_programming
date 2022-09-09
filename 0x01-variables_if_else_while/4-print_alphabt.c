39167c9ebaf848c4e4db8657a1becf982971fc77
Olawale Aro <aro.olawale1@gmail.com>
2022-09-09 15:32:39 -0700

    edited script for task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	alpha = 'a';
+	char alpha = 'a';
 
 	while (alpha <= 'z')
 	{
-		if (alpha == 'e' || alpha == 'q')
+		if ((alpha == 'e') || (alpha == 'q'))
 		{
 			continue;
 		}
