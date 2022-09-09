b1862035ccfb218c659cc8ffa1ef267df91d58a2
Olawale Aro <aro.olawale1@gmail.com>
2022-09-09 11:52:47 -0700

    added function doc and updated betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand(time (0));
 	n = rand() - RAND_MAX / 2;
 
 	if(n > 0)
 		printf("%u is positive\n", n);
-	else if (n < 0)
+	else if(n < 0)
 		printf("%u is negative\n", n);
 	else
 		printf("%u is zero\n", n);
