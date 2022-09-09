d92ffbacf1ad3e7e8e452616617a492c19f195a6
Olawale Aro <aro.olawale1@gmail.com>
2022-09-09 14:40:18 -0700

    task 2 added as 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase a-z
+ * Return: Always 0 (success)
+ */
+
+into main(void)
+{
+	char alpha = 'a';
+
+	while (apha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
