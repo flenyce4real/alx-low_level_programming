f4ec212c36307820eedcc005d717001c966029a7
Olawale Aro <aro.olawale1@gmail.com>
2022-09-09 16:43:24 -0700

    added task 8 and 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int n = '0';
-  
+
 	while (n <= '9')
 	{
 		putchar(n);
-    
+
 		if (n != '9')
 		{
 			putchar(',');
@@ -21,6 +21,6 @@ int main(void)
 		n++;
 	}
 	putchar('\n');
-  
+
 	return (0);
 }
