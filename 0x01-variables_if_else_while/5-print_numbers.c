4f99cc91f80b3e60c640ccfcc1c26a6f4ed782c7
Olawale Aro <aro.olawale1@gmail.com>
2022-09-09 15:53:11 -0700

    added task 5 that prints numbers 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (x = 0; x <= 9; x++)
+	{
+		printf(x);
+	}
+
+	printf('\n');
+
+	return (0);
+}	
