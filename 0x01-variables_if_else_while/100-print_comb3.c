3e12637afab6d271a318a8275d2abee3272cf4c9
Olawale Aro <aro.olawale1@gmail.com>
2022-09-12 15:00:31 -0700

    corrected task 10 (script 100)

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 /**
- *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
- *Return: Always 0 (Success)
+ * main - print all possible two digits combinations
+ * Return: Always 0 (success)
  */
- 
+
 int main(void)
 {
 	int ones;
 	int tens;
-  
-	for (tens = '0'; tens <= '9'; tens++) /* increment tens */
+
+	for (tens = 0; tens <= '9'; tens++)
 	{
-		for (ones = (tens + 1); ones <= '9'; ones++) /* one's ten +1 */
+		for (ones = 0; ones <= '9'; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
 
-			if (tens != '8' || ones != '9') /* print commas */
+			if (tens != '8' || ones != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
-  
+
 	return (0);
 }
